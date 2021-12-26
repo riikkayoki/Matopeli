b9cb56d23b3cdd94d767d4dae24b1ec81d35af67
Riikka <74421508+riikkayoki@users.noreply.github.com>
2021-12-26 21:50:25 +0200

    Update build.py

--- harjoitustyo/src/build.py
+++ harjoitustyo/src/build.py
@@ -4,7 +4,5 @@ from initialize_database import initialize_database
 def build():
     initialize_database()
 
-
-# This allows us to call the build function using command line
 if __name__ == '__main__':
     build()
